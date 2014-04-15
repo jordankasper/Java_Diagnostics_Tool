9fde1019a38d57c29baee051254117941603be63
Jordan Kasper <jordan@gmail.com>
2014-04-15 07:52:44 -0700

    Added to the diagstart.js file and finished process.

--- WebContent/diagStart.jsp
+++ WebContent/diagStart.jsp
@@ -77,81 +77,12 @@
 		<p>It looks like you are running <%out.println(application.getServerInfo()); %>, we are going to help you with some common troubleshooting techniques to try and get this up and running.  We are also including a list of your system information so that after we go through all of these steps you can
 			send us this information to help streamline your troubleshooting process.  Once you have completed the steps below restart had run this again, if you get back to this page hit the next button on the bottom to continue.</p>
 		
-		<div id="troubleOne"></div>
-        <p>Please save this information below and send to us after you complete the following steps.</p>
-        <p><b>Application Server :</b>  <%out.println(application.getServerInfo()); %></p>
-        <p> <b>OS Arch :</b><% out.println(System.getProperty("os.arch")); %></p>
-        <p><b>Command Line Arguments : </b><% 
-
-			  RuntimeMXBean runtimeMxBean = ManagementFactory.getRuntimeMXBean();
-			  List<String> arguments = runtimeMxBean.getInputArguments(); 
-			  String[] strarray = arguments.toArray(new String[0]);
-
-			  
-			  for(String s : strarray)
-				  out.println(s);
-			  %>
-		</p>
+		
+		<div id="troubleOne"><span id="serverType"><%out.println(application.getServerInfo()); %></span></div>
+       
 		<div id="troubleTwo"></div>
-		<p>Next you are going to need to collect your New Relic log file for use.  For this we would apperciate seeing a JVM restart or start up for this to see what happens when it starts.  Along with that setting them to finer level will also be extremely helpful in seeing what the
-			agent is doing.  Once you have collected about 5 minutes of logs you can turn finer off and then save them and send us this file along with the info from above.<p>
-		<p>To do this please perform the following:</p>
-		<ol>
-			<li>Open your newrelic.yml file</li>
-			<li>find the setting log_level:info</li>
-			<li>change this to: log_level:finer</li>
-			<li>save and exit newrelic.yml</li>
-			<li>exercise your app for a couple minutes.</li>
-			<li>send the newly generated log to us (The name of the log file is newrelic_agent.log and is generally in the "logs" directory which is created by the Agent in the same location as the newrelic.jar in the -javaagent switch (as long as that directory is writable by the JVM process)</li>
-		</ol>
-		<p>*note* Keep an eye on the log file size.  this can generate a lot of information very quickly, so it is best not to keep the agent at this level for longer than it takes to reproduce the problem you are experiencing.</p>
 		
 		<div id="troubleThree"></div>
-		<p>Once you have collected all of this information please bundle all of it and start a support ticket with us here at <a herf="www.support.newrelic.com">support.newrelic.com</a>.  When you do please make sure to attach what you have collected along with letting us know you have used the diagnostics tool, this will help us streamline your issue
-		 and hopefully cut down on resolution time with you.</p>
-		 <br>
-		 <br>	
-
-
-		
-		<h4>System Information</h4>
-		<br>
-		
-		<p><b>Application Server :</b>  <span id="serverType"><%out.println(application.getServerInfo()); %></span></p>
-		
-		<p><b>Current Directory : </b><%File file = new File("."); out.println(file.getCanonicalPath());%> </p>
-		
-		<p> <b>CL Path Set : </b><% out.println("-s " + file.getCanonicalPath());%></p> 
-		
-		<p> <b>OS Arch :</b><% out.println(System.getProperty("os.arch")); %></p>
-		
-		<p> <b>Java Class Path :</b><% out.println(System.getProperty("java.class.path")); %></p>
-		
-		<p> <b>Java Home :</b><% out.println(System.getProperty("java.home")); %></p>
-		
-		<p> <b>Vendor :</b><% out.println(System.getProperty("java.vendor")); %></p>
-	
-		<p> <b>Java Version :</b><% out.println(System.getProperty("java.version")); %></p>
-		
-		<p> <b>OS Name :</b><% out.println(System.getProperty("os.name")); %></p>
-
-		<p> <b>Directory :</b><% out.println(System.getProperty("user.dir")); %></p>
-	
-		<p> <b>Host :</b><% out.println(request.getServerName()); %></p>
-
-		<p> <b>Port :</b><% out.println(request.getServerPort()); %></p>
-		
-		
-		<p><b>Command Line Arguments : </b><% 
-
-			strarray = arguments.toArray(new String[0]);
-
-		  
-		  	for(String s : strarray)
-			  out.println(s);
-			  
-			  %>
-		 </p> 
 		<br><br>
 		
 		<script>
--- WebContent/diagtool.js
+++ WebContent/diagtool.js
@@ -6,22 +6,64 @@ $(document).ready(function() {
 	if(server.match(/tomcat/i)){
 		console.log(server);
 		$("#selectedServer").html("<h1>Tomcat Installation Troubleshooting</h1>");
-		$("#troubleOne").html("<h2>Step 1</h2>");
-		$("#troubleTwo").html("<h2>Step 2</h2>");
-		$("#troubleThree").html("<h2>Step 3</h2>");
+		$("#troubleOne").html("<h2>Step 1 </h2>" +
+				"<p> First lets make sure you are installing using the correct steps, with Tomcat you can use the auto installer which should be the easiest way to do this.  These steps can be found here <a href='https://docs.newrelic.com/docs/java/java-agent-self-installer'>Java Agent self installer</a>" +
+				"<p> Unless you have special install considerations, you should be able to complete your New Relic install by following the directions on Get started with Web App Monitoring. This page provides additional information on the install process." +
+				"</h2>");
+		$("#troubleTwo").html("<h2>Step 2</h2>" +
+				"The Tomcat startup script (catalina.sh) can be configured to use the New Relic agent using the JAVA_OPTS environment variable: export JAVA_OPTS='$JAVA_OPTS -javaagent:/full/path/to/newrelic.jar' The -javaagent switch should reference the full path to the newrelic.jar file." +
+				"" +
+				"");
+		$("#troubleThree").html("<h2>Step 3</h2>" +
+				"<p>Once you have that set up make sure you have read and write permissions to the folders New Relic needs to access.  Once you have that restart your application server and try running New Relic again.  If this does not work after running this continue to the next page so we can continue to troubleshoot this with you.");
 
 	}
+	
+	
 	else if(server.match(/jetty/i)){
-		
+		$("#selectedServer").html("<h1>Jetty Installation Troubleshooting</h1>");
+		$("#troubleOne").html("<h2>Step 1 </h2>" +
+				"<p> First lets make sure you are installing using the correct steps, with Jetty you can use the auto installer which should be the easiest way to do this.  These steps can be found here <a href='https://docs.newrelic.com/docs/java/java-agent-self-installer'>Java Agent self installer</a>" +
+				"<p> Unless you have special install considerations, you should be able to complete your New Relic install by following the directions on Get started with Web App Monitoring. This page provides additional information on the install process." +
+				"</h2>");
+		$("#troubleTwo").html("<h2>Step 2</h2>" +
+				"The Jetty startup script (jetty.sh) can be configured using the JAVA_OPTIONS environment variable: export JAVA_OPTIONS='${JAVA_OPTIONS} -javaagent:/full/path/to/newrelic.jar' The -javaagent switch should reference the full path to the newrelic.jar file.");
+		$("#troubleThree").html("<h2>Step 3</h2>" +
+				"<p>Once you have that set up make sure you have read and write permissions to the folders New Relic needs to access.  Once you have that restart your application server and try running New Relic again.  If this does not work after running this continue to the next page so we can continue to troubleshoot this with you.");
 	}
+	
+	
 	else if(server.match(/jboss/i)){
-		
+		$("#selectedServer").html("<h1>Jboss Installation Troubleshooting</h1>");
+		$("#troubleOne").html("<h2>Step 1 </h2>" +
+				"<p> First lets make sure you are installing using the correct steps, with Jboss you can use the auto installer which should be the easiest way to do this.  These steps can be found here <a href='https://docs.newrelic.com/docs/java/java-agent-self-installer'>Java Agent self installer</a>" +
+				"<p> Unless you have special install considerations, you should be able to complete your New Relic install by following the directions on Get started with Web App Monitoring. This page provides additional information on the install process." +
+				"</h2>");
+		$("#troubleTwo").html("<h2>Step 2</h2>" +
+				"<p>With manual installation for Jboss there are two ways you can do it, first is Domain mode and the second is Standalone mode.  For Domain mode The domain mode directions are relevant to JBoss versions 6.x EAP or 7.0.x AS and above.  The JVM properties for each server group are found in domain/configuration/domain.xml." +
+				"The JVM properties for each server group must be modified to include the -javaagent flag.  Be sure the -javaagent switch references the full path to the newrelic.jar file.  If you are installing on Windows, be sure to use forward slashes. For example: C:/newrelic/newrelic.jar.</p>" +
+				"<p> With stand alone mode there are a few different ways you can do it depending on what Platform you are using.  These can be viewed here <a href='https://docs.newrelic.com/docs/java/jboss-installation-for-java'>Jboss Stand Alone Configuration </a> </p> ");
+		$("#troubleThree").html("<h2>Step 3</h2>" +
+				"<p>Once you have that set up make sure you have read and write permissions to the folders New Relic needs to access.  Once you have that restart your application server and try running New Relic again.  If this does not work after running this continue to the next page so we can continue to troubleshoot this with you.");
 	}
+	
+	
 	else if(server.match(/glassfish/i)){
-		
+		$("#selectedServer").html("<h1>Glashfish Installation Troubleshooting</h1>");
+		$("#troubleOne").html("<h2>Step 1 </h2>" +
+				"<p> First lets make sure you are installing using the correct steps, with Glassfish you can use the auto installer which should be the easiest way to do this.  These steps can be found here <a href='https://docs.newrelic.com/docs/java/java-agent-self-installer'>Java Agent self installer</a>" +
+				"<p> Unless you have special install considerations, you should be able to complete your New Relic install by following the directions on Get started with Web App Monitoring. This page provides additional information on the install process." +
+				"</h2>");
+		$("#troubleTwo").html("<h2>Step 2</h2>" +
+				"<p>To install the New Relic java agent on Glassfish:</p>" +
+				"<p>Access the Glassfish console, in the left hand tree, select Application Server > JVM Settings tab > JVM Options.  In the left hand tree, select Application Server > JVM Settings tab > JVM Options.  On the JVM Options page, select Add JVM Option.  Add an entry for the -javaagent switch. -javaagent:/full/path/to/newrelic.jar Select Save. Restart Glassfish.</p>");
+		$("#troubleThree").html("<h2>Step 3</h2>" +
+				"<p>Once you have that set up make sure you have read and write permissions to the folders New Relic needs to access.  Once you have that restart your application server and try running New Relic again.  If this does not work after running this continue to the next page so we can continue to troubleshoot this with you.");
 	}
 	else{
-		
+		$("#selectedServer").html("<h1>Unknown Application or Web Server</h1>");
+		$("#troubleOne").html("<h2>Step 1 </h2>" +
+				"<p> It looks like we do not reconize the application or webserver you are trying to run this on, make sure you are using either Tomcat, Jetty, Jboss or Glassfish.  If you are not using on of those please feel free to access our community forum at <a href='http://discuss.newrelic.com/'>New Relic Community Forums </a> </p>");
 	}
       
       
